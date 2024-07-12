fa9b6767c83b83568e326b97ed08f0ee5f48a684
unknown <susen.huseynova.11@mail.ru>
2024-07-12 12:38:46 +0400

    fix:scss/layout/footer

--- src/js/app.js
+++ src/js/app.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
         sidebar.classList.toggle('active');
     });
 
-    // Close sidebar when clicking outside of it
+   
     document.addEventListener('click', function(event) {
         if (!sidebar.contains(event.target) && event.target !== shopLink) {
             sidebar.classList.remove('active');
@@ -54,3 +54,58 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+let cart = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProducts();
+});
+
+
+function fetchProducts() {
+    fetch('http://localhost:3001/products')
+        .then(response => response.json())
+        .then(data => {
+            const products = data.products;
+            renderProducts(products);
+        })
+        .catch(error => console.error('Error fetching products:', error));
+}
+
+
+function renderProducts(products) {
+    const mainCardSection = document.querySelector('.main-card-image');
+
+    products.forEach(product => {
+        const cardWordSection = document.createElement('div');
+        cardWordSection.classList.add('card-word-section');
+        cardWordSection.innerHTML = `
+            <div class="card1">
+                <div class="image-container">
+                    <img src="${product.image}" alt="${product.name}" class="main-image">
+                    <img src="${product.hoverImage}" alt="${product.name} Hover" class="hover-image">
+                    <div class="overlay">
+                        <div class="icons">
+                            <div class="icon search"></div>
+                            <div class="icon heart"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="details">
+                    <h3 class="h3">${product.name}</h3>
+                    <div class="rating">
+                        <span class="star-card">★★★★☆</span>
+                    </div>
+                    <div class="price">
+                        <span class="original-price">$98.00</span>
+                        <span class="discounted-price">$${product.price.toFixed(2)}</span>
+                    </div>
+                    <button class="select-options" onclick="addToCart(${product.id})">Add to cart</button>
+                </div>
+            </div>
+        `;
+        mainCardSection.appendChild(cardWordSection);
+    });
+}
+
+renderProducts(products);
+fetchProducts();
