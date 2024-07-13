b16539db2b84bc1f8da7fb666fc509f8b45470e6
unknown <susen.huseynova.11@mail.ru>
2024-07-13 11:39:18 +0400

    fetured:public/care.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    new Swiper(".custom-card-slider", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".swiper-next",
-            prevEl: ".swiper-prev",
-        },
-        pagination: {
-            el: ".swiper-page",
-            clickable: true,
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-        breakpoints: {
-            1400: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            992: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            576: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            350: {
-                slidesPerView: 1,
-                spaceBetween: 2,
-            },
-        },
-    });
-});
 // Function to open or close the sidebar
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
@@ -96,27 +60,31 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   
-const cardsContainer = document.querySelector('.cards');
-
-
-fetch('http://localhost:3001/cards')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through each card data object
-    data.cards.forEach(card => {
-      // Create card HTML structure
-      const cardHTML = `
-        <div class="card">
-          <img src="${card.image}" alt="">
-          <div class="overlay">
-            <i class="fa-regular fa-heart"></i><span class="count">${card.likes}</span>
-            <i class="fa-regular fa-comment"></i><span class="count">${card.comments}</span>
+  
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:3001/cards");
+      const data = await response.json();
+      console.log(data);
+  
+      const cardContainer = document.querySelector(".maincardcontainerfromapi");
+  
+      data.cards.forEach((card) => {
+        cardContainer.innerHTML += `
+          <div class="card">
+            <img src="${card.image}" alt="">
+            <div class="overlay">
+              <i class="fa-regular fa-heart"></i><span class="count">${card.likes}</span>
+              <i class="fa-regular fa-comment"></i><span class="count">${card.comments}</span>
+            </div>
           </div>
-        </div>
-      `;
-      
-      // Append card HTML to cards container
-      cardsContainer.innerHTML += cardHTML;
-    });
-  })
-  .catch(error => console.error('Error fetching data:', error));
+        `;
+      });
+  
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  fetchData();
+  
---
+++ src/js/swipper.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new Swiper(".custom-card-slider", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
+        },
+        pagination: {
+            el: ".swiper-page",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            1400: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            992: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            576: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            350: {
+                slidesPerView: 1,
+                spaceBetween: 2,
+            },
+        },
+    });
+});
+
+
